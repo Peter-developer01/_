c208a2aa72848808d7ceef3fe15603ecef34f07b
Peter <101456132+Peter-developer01@users.noreply.github.com>
2025-03-15 15:33:49 +0300

    fix bug

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ def on_message(msg, client):
 						room.send_message(v)
 				else: room.send_message(reply)
 			else:
-				if (message.content.startswith(config.COMMAND_PREFIX + "hang") or message.content.startswith(config.COMMAND_PREFIX + "hang")):
+				if (message.content.lower().startswith(config.COMMAND_PREFIX + "hang") or message.content.lower().startswith(config.COMMAND_PREFIX + "hang")):
 					room.send_message(reply.replace("\\*", "*"))
 				else:
 					reply = md(reply).replace("\\*", "*")
